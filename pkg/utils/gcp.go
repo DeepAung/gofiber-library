accd0db5ed5d4148d9b2aab721224fb1ae70378f
DeepAung <i.deepaung@gmail.com>
2024-04-24 13:32:01 +0700

    style: create page, fix: url space to %20

--- pkg/utils/gcp.go
+++ pkg/utils/gcp.go
@@ -7,12 +7,16 @@ import (
 
 var website = "https://storage.googleapis.com"
 
-func MakeGCPUrl(bucket string, dest string) string {
-	return fmt.Sprintf("%s/%s/%s", website, bucket, dest)
+func MakeGCPUrl(bucket string, dest string) (url string) {
+	url = fmt.Sprintf("%s/%s/%s", website, bucket, dest)
+	url = strings.ReplaceAll(url, " ", "%20")
+	return url
 }
 
 func GetUrlAndFilename(bucket, dest string) (url string, filename string) {
 	url = website + "/" + bucket + "/" + dest
+	url = strings.ReplaceAll(url, " ", "%20")
+
 	splits := strings.Split(dest, "/")
 	filename = splits[len(splits)-1]
 
